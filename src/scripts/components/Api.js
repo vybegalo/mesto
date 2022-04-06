f20a99b5fd856c4641f7bfe2f83df72affa53cb2
Evgeniya Kovaleva <kesha@sytkovo.su>
2022-04-06 15:50:15 +0700

    Save and fetch user profile from server

---
+++ src/scripts/components/Api.js
@@ -0,0 +1,79 @@
+
+export default class Api {
+    constructor({ baseUrl, headers }) {
+        this._baseUrl = baseUrl;
+        this._headers = headers;
+    }
+
+    _handleResponse(res) {
+        if (res.ok) return res.json();
+        return Promise.reject(`Error: ${res.status}`);
+    }
+
+    getInitialCards() {
+        return fetch(`${this._baseUrl}/cards`, {
+            method: 'GET',
+            headers: this._headers,
+        })
+            .then((res) => this._handleResponse(res));
+    }
+
+    getUserInfo() {
+        return fetch(`${this._baseUrl}/users/me`, {
+            method: 'GET',
+            headers: this._headers,
+        })
+            .then((res) => this._handleResponse(res));
+    }
+
+    updateUserAvatar(link) {
+        return fetch(`${this._baseUrl}/users/me/avatar`, {
+            method: 'PATCH',
+            headers: this._headers,
+            body: JSON.stringify(link),
+        })
+            .then((res) => this._handleResponse(res));
+    }
+
+    updateUserProfile(data) {
+        return fetch(`${this._baseUrl}/users/me`, {
+            method: 'PATCH',
+            headers: this._headers,
+            body: JSON.stringify({ name: data.userName, about: data.userInfo }),
+        })
+            .then((res) => this._handleResponse(res));
+    }
+
+    addLike(cardId) {
+        return fetch(`${this._baseUrl}/cards/likes/${cardId}`, {
+            method: 'PUT',
+            headers: this._headers,
+        })
+            .then((res) => this._handleResponse(res));
+    }
+
+    removeLike(cardId) {
+        return fetch(`${this._baseUrl}/cards/likes/${cardId}`, {
+            method: 'DELETE',
+            headers: this._headers,
+        })
+            .then((res) => this._handleResponse(res));
+    }
+
+    addNewPlace(item) {
+        return fetch(`${this._baseUrl}/cards`, {
+            method: 'POST',
+            headers: this._headers,
+            body: JSON.stringify(item),
+        })
+            .then((res) => this._handleResponse(res));
+    }
+
+    deletePlace(cardId) {
+        return fetch(`${this._baseUrl}/cards/${cardId}`, {
+            method: 'DELETE',
+            headers: this._headers,
+        })
+            .then((res) => this._handleResponse(res));
+    }
+}
--- src/scripts/components/UserInfo.js
+++ src/scripts/components/UserInfo.js
@@ -1,7 +1,9 @@
 export default class UserInfo {
-    constructor({ userName, userInfo }) {
+    constructor({ userName, userInfo, userAvatar }) {
         this._userName = userName;
         this._userInfo = userInfo;
+        this._userAvatar = userAvatar;
+        this._userID = 0;
     }
 
     getUserInfo() {
@@ -14,6 +16,22 @@ export default class UserInfo {
     setUserInfo(userData) {
         this._userName.textContent = userData.userName;
         this._userInfo.textContent = userData.userInfo;
+    }
+
+    getUserID() {
+        return this._userID;
+    }
+
+    setUserID(id) {
+        this._userID = id;
+    }
+
+    getUserAvatar() {
+        return this._userAvatar.src;
+    }
+
+    setUserAvatar(link) {
+        this._userAvatar.src = link;
     }
 
 }
