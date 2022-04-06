ac003891f01582b86c29e893ef65fa8cc30411dd
Evgeniya Kovaleva <kesha@sytkovo.su>
2022-04-06 15:50:15 +0700

    Save and fetch user profile from server

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import "./index.css";
 import "../index.html";
+import Api from "../scripts/components/Api.js";
 import Card from "../scripts/components/Card.js";
 import FormValidator from "../scripts/components/FormValidator.js";
 import PopupWithImage from "../scripts/components/PopupWithImage.js";
@@ -9,13 +10,23 @@ import UserInfo from "../scripts/components/UserInfo.js";
 import { initialCards } from "../scripts/utils/icards.js";
 import {
     buttonEditProfile, buttonClose, popupEditProfile,
-    userName, userInfo, nameInput,
-    jobInput, placeAddButton, popupAddPlace,
+    buttonSubmitProfile, userName, userInfo, userAvatar, nameInput,
+    jobInput, popupChangeAvatar, popupChangeAvatarForm,
+    buttonSubmitAvatar, placeAddButton, popupAddPlace,
     popupImage, elementsContainer, elementSelector,
     popupEditProfileForm, popupAddPlaceForm, formsValidationConfig
 } from "../scripts/utils/constants.js";
 
+let placesList = [];
+let placesListSection = null;
 
+const api = new Api({
+    baseUrl: 'https://mesto.nomoreparties.co/v1/cohort-38',
+    headers: {
+        authorization: '8f3f41bd-1c8a-4382-89e9-88174d98da09',
+        'Content-Type': 'application/json',
+    },
+});
 
 // Validation section
 
@@ -25,25 +36,42 @@ const createValidatorInstance = (config, form) => {
 };
 
 /* enable forms validation */
-
 const profileFormValidation = createValidatorInstance(formsValidationConfig, popupEditProfileForm);
 profileFormValidation.enableValidation();
-
 const addPlaceValidation = createValidatorInstance(formsValidationConfig, popupAddPlaceForm);
 addPlaceValidation.enableValidation();
+const updateUserAvatarValidation = createValidatorInstance(formsValidationConfig, popupChangeAvatarForm);
+updateUserAvatarValidation.enableValidation();
 
 
 // Profile manage section
 
 const userInfoInstance = new UserInfo({
     userName: userName,
-    userInfo: userInfo
+    userInfo: userInfo,
+    userAvatar: userAvatar
 });
+
+/* save user data on server */
+const saveUserProfile = (data) => {
+    buttonSubmitProfile.textContent = 'Сохранение...';
+    api.updateUserProfile(data)
+        .then((res) => {
+            userInfoInstance.setUserInfo({ userName: res.name, userInfo: res.about });
+            userInfoInstance.setUserID(res._id);
+        })
+        .catch((err) => {
+            console.log(`Cannot update user profile: ${err}`);
+        })
+        .finally(() => {
+            editProfileInstance.close();
+            buttonSubmitProfile.textContent = 'Сохранить';
+        });
+}
 
 const editProfileInstance = new PopupWithForm(popupEditProfile, {
     submitFormCallback: (data) => {
-        userInfoInstance.setUserInfo(data);
-        editProfileInstance.close();
+        saveUserProfile(data);
     }
 });
 
@@ -57,6 +85,43 @@ buttonEditProfile.addEventListener('click', () => {
     jobInput.value = userData.userInfo;
     profileFormValidation.resetValidation();
 });
+
+buttonClose.addEventListener('click', () => {
+    editProfileInstance.close();
+});
+
+/* update avatar popup */
+const popupChangeAvatarInstance = new PopupWithForm(popupChangeAvatar, {
+    submitFormCallback: (data) => {
+        saveUserAvatar(data);
+        popupChangeAvatarInstance.close();
+    }
+});
+
+/* save user avatar */
+const saveUserAvatar = (data) => {
+    buttonSubmitAvatar.textContent = 'Сохранение...';
+    api.updateUserAvatar(data)
+        .then((res) => {
+            userInfoInstance.setUserAvatar(res.avatar);
+            userInfoInstance.setUserID(res._id);
+        })
+        .catch((err) => {
+            console.log(`Cannot update user avatar: ${err}`);
+        })
+        .finally(() => {
+            popupChangeAvatarInstance.close();
+            buttonSubmitAvatar.textContent = 'Сохранить';
+        });
+}
+
+userAvatar.addEventListener('click', () => {
+    popupChangeAvatarInstance.open(userInfoInstance.getUserAvatar());
+    updateUserAvatarValidation.resetValidation();
+});
+
+popupChangeAvatarInstance.setEventListeners();
+
 
 // Enlarge image section
 
@@ -77,21 +142,12 @@ const createCardInstance = (element) => {
 };
 
 /* place renderer */
-
 const renderElement = (item, toBeggining) => {
     const element = createCardInstance(item);
     placesListSection.addItem(element, toBeggining);
 };
 
-/* places Section */
-
-const placesListSection = new Section(
-    { items: initialCards, renderer: renderElement },
-    elementsContainer
-);
-
 /* create place */
-
 const newPlacePopup = new PopupWithForm(popupAddPlace, {
     submitFormCallback: (data) => {
         const elementData = {
@@ -107,13 +163,48 @@ const newPlacePopup = new PopupWithForm(popupAddPlace, {
 
 newPlacePopup.setEventListeners();
 
-/* initialize cards */
-
-placesListSection.renderElements();
-
 /* add place popup */
-
 placeAddButton.addEventListener('click', () => {
     newPlacePopup.open();
     addPlaceValidation.resetValidation();
 });
+
+
+// Fetch data from server section
+
+api.getUserInfo()
+    /* get user data */
+    .then((res) => {
+        userInfoInstance.setUserInfo({ userName: res.name, userInfo: res.about });
+        userInfoInstance.setUserAvatar(res.avatar);
+        userInfoInstance.setUserID(res._id);
+    })
+    .catch((err) => {
+        console.log(`Cannot get user information: ${err}`);
+    })
+    .finally(() => {
+        /* get places cards */
+        api.getInitialCards()
+            .then((res) => {
+                placesList = res.map((item) => ({
+                    name: item.name,
+                    alt: item.name,
+                    link: item.link,
+                    likes: item.likes,
+                    owner: item.owner._id,
+                    id: item._id,
+                }));
+            })
+            .catch((err) => {
+                console.log(`Cannot load cards from server: ${err}`);
+                placesList = initialCards;
+            })
+            .finally(() => {
+                /* render cards */
+                placesListSection = new Section(
+                    { items: placesList, renderer: renderElement },
+                    elementsContainer
+                );
+                placesListSection.renderElements();
+            });
+    });
